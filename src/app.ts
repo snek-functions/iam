c22e33d1e726a59de0c2303ffb8f8fe79d370ece
Nico Schett <52858351+schettn@users.noreply.github.com>
2022-10-18 14:58:57 +0000

    feat: add configureApp with cors

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,22 @@
-import getServerlessApp from '@snek-at/functions/dist/server/getServerlessApp.js'
+import cors from "cors";
+import getServerlessApp from "@snek-at/functions/dist/server/getServerlessApp.js";
+
+import { ConfigureApp } from "@snek-at/functions";
 
 export async function handler(event: Object, context: Object) {
   return await getServerlessApp({
-    functions: '.'
-  })(event, context)
+    functions: ".",
+  })(event, context);
 }
+
+export const configureApp: ConfigureApp = (app) => {
+  app.use((req, res, next) => {
+    return cors({
+      origin: true,
+      credentials: true,
+    })(req, res, next);
+  });
+};
 
 // SPDX-License-Identifier: (EUPL-1.2)
 // Copyright © 2019-2022 snek.at
