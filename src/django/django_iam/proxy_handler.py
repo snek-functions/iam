21d7272fe05d23dcdb00a7f689f7299b80dc0bb8
Florian Kleber <kleberbaum@erebos.xyz>
2022-10-18 21:14:26 +0000

    fix: NoneType no attribute upper

--- src/django/django_iam/proxy_handler.py
+++ src/django/django_iam/proxy_handler.py
@@ -59,8 +59,8 @@ def function_handler(message: ProxyMessage):
         user_id = message.data.get("userId")
         email = message.data.get("email")
         password = message.data.get("password")
-        first_name = message.data.get("firstName").upper()
-        last_name = message.data.get("lastName").upper()
+        first_name = message.data.get("firstName", "").upper()
+        last_name = message.data.get("lastName", "").upper()
         is_active = message.data.get("isActive") == True
 
         print(f"Is Active test: {message.data.get('isActive')}")
